13ee57d9932391c179e72ed375bd3ca6d1cc250f
nishantcg13 <galandesonu42@gmail.com>
2026-01-02 23:38:07 +0530

    RESTAPI

---
+++ bootdevtool/bootdevtool/src/main/java/com/test/bootdevtool/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.test.bootdevtool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "HEllo mY NAME IS NISHANTSSSS SANIKA";
+    }
+}
