ec2435990649a47af1dfde4203eafbee08a893b8
nishantcg13 <galandesonu42@gmail.com>
2026-01-12 10:15:50 +0530

    FILE HANDLING

--- bootrestbook/bootrestbook/src/main/java/com/rest/bootrestbook/controller/FileUploadController.java
+++ bootrestbook/bootrestbook/src/main/java/com/rest/bootrestbook/controller/FileUploadController.java
@@ -1,35 +1,61 @@
 package com.rest.bootrestbook.controller;
 
+import com.rest.bootrestbook.helper.FileUploadHelper;
 import jakarta.annotation.Nonnull;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class FileUploadController {
 
+    @Autowired
+    private FileUploadHelper fileUploadHelper;
+
+    @Autowired
+    HttpServletRequest request;
 
     @PostMapping("/file")
     public ResponseEntity<String> fileUpload( @RequestParam("file")MultipartFile file) {
 
+        try {
 
-        //validation
-        if(file.isEmpty()){
-            return ResponseEntity.internalServerError().body("File should not be empty");
+
+            //validation
+            if (file.isEmpty()) {
+                return ResponseEntity.internalServerError().body("File should not be empty");
+            }
+
+            if (!file.getContentType().equals("image/jpeg")) {
+                return ResponseEntity.internalServerError().body("Only JPEG content type allowed");
+            }
+
+            //file upload code
+            boolean result = fileUploadHelper.uploadFile(file);
+
+            System.out.println(file.getOriginalFilename());
+            System.out.println(file.getContentType());
+            System.out.println(file.getSize());
+            System.out.println(file.getName());
+
+            if(result) {
+//                return ResponseEntity.ok("File Uploaded Successfully");
+//            now i want to return file-path to the user so
+                return ResponseEntity.ok(ServletUriComponentsBuilder.fromContextPath(request).path("/image/").path(file.getOriginalFilename()).toUriString());
+
+            }else{
+                return ResponseEntity.internalServerError().body("Something went wrong ... Try Again !!!");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
-        if(!file.getContentType().equals("image/jpeg")){
-            return ResponseEntity.internalServerError().body("Only JPEG content type allowed");
-        }
-
-        System.out.println(file.getOriginalFilename());
-        System.out.println(file.getContentType());
-        System.out.println(file.getSize());
-        System.out.println(file.getName());
-
-        return ResponseEntity.ok("File Uploaded Successfully");
+        return ResponseEntity.ok("Working");
     }
 
 }
--- bootrestbook/bootrestbook/src/main/java/com/rest/bootrestbook/helper/FileUploadHelper.java
+++ bootrestbook/bootrestbook/src/main/java/com/rest/bootrestbook/helper/FileUploadHelper.java
@@ -1,16 +1,29 @@
 package com.rest.bootrestbook.helper;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 @Component
 public class FileUploadHelper {
 
-    public final String UPLOAD_DIR = "C:`\\`Users\\galan\\Desktop\\Spring Boot\\bootrestbook\\bootrestbook\\src\\main\\resources\\static\\image";
+
+    //this is static path and it will give error when the pc or server will be changed to inorder to make it dynamic the
+    // below line is given
+
+//    public final String UPLOAD_DIR = "C:\\Users\\galan\\Desktop\\Spring Boot\\bootrestbook\\bootrestbook\\src\\main\\resources\\static\\image";
+    public final String UPLOAD_DIR = new ClassPathResource("static/image").getFile().getAbsolutePath();
+    //this will throw the exception so handle it in this class constructor
+
+    FileUploadHelper() throws IOException {
+
+    }
+
 
 
     public boolean uploadFile(MultipartFile file){
@@ -18,18 +31,20 @@ public class FileUploadHelper {
 
         try{
 
-            //read
-            InputStream is = file.getInputStream();
-            byte data[] = new byte[is.available()];
-            is.read(data);
+//            //read
+//            InputStream is = file.getInputStream();
+//            byte data[] = new byte[is.available()];
+//            is.read(data);
+//
+//            //write
+//            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+"\\"+file.getOriginalFilename());
+//            // here we can use File.separator instead of "//" in the code
+//
+//            fos.write(data);
+//            fos.flush();
+//            fos.close();
 
-            //write
-            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+"\\"+file.getOriginalFilename());
-            // here we can use File.separator instead of "//" in the code
-
-            fos.write(data);
-            fos.flush();
-            fos.close();
+            Files.copy(file.getInputStream(), Path.of(UPLOAD_DIR+ File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
 
             result = true;
         }catch(Exception e){
