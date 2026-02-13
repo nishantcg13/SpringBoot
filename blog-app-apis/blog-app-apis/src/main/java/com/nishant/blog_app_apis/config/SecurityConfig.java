4521d2253ce0000d7cff0ddeffdb63bae73e2115
nishantcg13 <galandesonu42@gmail.com>
2026-02-14 00:46:27 +0530

    BLOG APP BACKEND 1-2

---
+++ blog-app-apis/blog-app-apis/src/main/java/com/nishant/blog_app_apis/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+//package com.nishant.blog_app_apis.config;
+//
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//
+
+
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+ //
+
+//}
+
+
+
