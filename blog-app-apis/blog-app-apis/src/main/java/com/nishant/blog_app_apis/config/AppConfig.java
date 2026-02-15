b62f1cc5d9f19d977573e641869a63efa0b7182d
nishantcg13 <galandesonu42@gmail.com>
2026-02-15 14:43:48 +0530

    BLOG APP APIS "15-02"

---
+++ blog-app-apis/blog-app-apis/src/main/java/com/nishant/blog_app_apis/config/AppConfig.java
@@ -0,0 +1,26 @@
+package com.nishant.blog_app_apis.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public UserDetailsService userDetailService() {
+
+        UserDetails user1 = User.builder().username("nishant").password("password").roles("ADMIN").build();
+        UserDetails user2 = User.builder().username("nishant").password("password").roles("ADMIN").build();
+        UserDetails user3 = User.builder().username("nishant").password("password").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user3);
+
+    }
+
+
+}
